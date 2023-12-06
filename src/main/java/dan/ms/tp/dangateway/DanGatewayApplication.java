2f575e982e4aebb14cefa9288dc14d87acf9c42d
mart-dominguez <mdomingu@gmail.com>
2023-12-05 22:07:07 -0300

    init

---
+++ src/main/java/dan/ms/tp/dangateway/DanGatewayApplication.java
@@ -0,0 +1,13 @@
+package dan.ms.tp.dangateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanGatewayApplication.class, args);
+	}
+
+}
